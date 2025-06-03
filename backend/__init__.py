c4b6090584f0e83f7cd58318636197202c154c51
Zain Nasir <zainasir1999@gmail.com>
2025-06-03 15:26:12 -0400

    finished structuring output response

---
+++ backend/__init__.py
@@ -0,0 +1,1 @@
+from .rag import run_rag
--- backend/rag.py
+++ backend/rag.py
@@ -1,4 +1,5 @@
 import getpass
+import json
 import os
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
@@ -8,58 +9,68 @@ from langchain.chat_models import init_chat_model
 from langchain.prompts import PromptTemplate
 from langgraph.graph import START, StateGraph
 
-if not os.environ.get("OPENAI_API_KEY"):
-    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
-else:
-    print("OpenAI Key found!")
-
-
-prompt = PromptTemplate.from_template(
-    """
-    You are an assistant for question-answering tasks related to cBioPortal publications. Use the following extracted content from cBioPortal publications to answer the question. If you don't know the answer, just say that you don't know.
-    
-    ---
-    Context:
-    {context}
-    ---
-    
-    Question:
-    {question}
-    
-    Answer:
-    """
-)
-persist_directory = "./data/vectordb/chroma/pubmed/paper_and_pdf"
-embedding_function = OpenAIEmbeddings(model="text-embedding-ada-002")
-llm = init_chat_model("gpt-4o-mini", model_provider="openai")
-
-vector_store = Chroma(
-    persist_directory=persist_directory,
-    embedding_function=embedding_function
-)
-
-print("Number of documents:", len(vector_store._collection.get()['ids']))
-
 class State(TypedDict):
     question: str
     context: List[Document]
     answer: str
 
-def retrieve(state: State):
-    retrieved_docs = vector_store.similarity_search(state["question"])
-    return {"context": retrieved_docs}
+def run_rag(user_prompt: str):
+    def retrieve(state: State):
+        retrieved_docs = vector_store.similarity_search(state["question"])
+        return {"context": retrieved_docs}
 
-def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = llm.invoke(messages)
-    return {"answer": response.content}
+    def generate(state: State):
+        docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+        messages = prompt.invoke({"question": state["question"], "context": docs_content})
+        response = llm.invoke(messages)
+        return {"answer": response.content}
 
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
-graph = graph_builder.compile()
+    prompt = PromptTemplate.from_template(
+        """
+        You are an assistant for question-answering tasks related to cBioPortal publications. Use the following context from cBioPortal publications to answer the question. If you don't know the answer, just say that you don't know. In your response, don't mention the word 'context' or refer to the context explicitly. Just provide an answer.
+        
+        ---
+        Context:
+        {context}
+        ---
+        
+        Question:
+        {question}
+        
+        Answer:
+        """
+    )
+    persist_directory = "./data/vectordb/chroma/pubmed/paper_and_pdf"
+    embedding_function = OpenAIEmbeddings(model="text-embedding-ada-002")
+    llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 
-result = graph.invoke({"question": "What pathways are altered in breast cancer."})
+    vector_store = Chroma(
+    persist_directory=persist_directory,
+    embedding_function=embedding_function
+    )
 
-print(f'Context: {result["context"]}\n\n')
-print(f'Answer: {result["answer"]}')
+    graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+    graph_builder.add_edge(START, "retrieve")
+    graph = graph_builder.compile()
+
+    result = graph.invoke({"question": user_prompt})
+
+
+    cbioportal_study_url = "https://www.cbioportal.org/study/summary?id="
+    filtered_metadata = [
+        {
+            "name": doc.metadata.get("name"),
+            "studyId": doc.metadata.get("studyId"),
+            "url": cbioportal_study_url + doc.metadata.get("studyId")
+        }
+        for doc in result["context"]
+    ]
+
+    result = result["answer"] + "\n\n"
+    result = result + "Citations:\n"
+    for doc in filtered_metadata:
+        result = result + f"* [{doc.get('name')}]({doc.get('url')})\n"
+
+    print(result)
+
+run_rag("Which studies are about gliomas?")
